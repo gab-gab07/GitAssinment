8f1df72437eacd468f2ec7ede068c61342e79410
gaby07-s <asaboia@email.tjc.edu>
2024-09-03 09:22:50 -0500

    Update Code.cpp
    You put commas instead of semicolons

--- Code.cpp
+++ Code.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-	cout << "never going to give you up" << endl,
-		cout << "never going to let you down" << endl,
-		cout << "never going to run around" << endl,
-		cout << "or desert you" << endl,
+	cout << "never going to give you up" << endl;
+		cout << "never going to let you down" << endl;
+		cout << "never going to run around" << endl;
+		cout << "or desert you" << endl;
 }
