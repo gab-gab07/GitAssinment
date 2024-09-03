6d89ae5198a03d12770796fa7f81f980003c39b2
gabys <gabys@GABYPC>
2024-09-03 09:18:46 -0500

    Add project files.

---
+++ Code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "never going to give you up" << endl,
+		cout << "never going to let you down" << endl,
+		cout << "never going to run around" << endl,
+		cout << "or desert you" << endl,
+}
